8cb32d45a7b272986ebd247b7b7d80ff09d95005
chchch <charles.li@cnrs.fr>
2024-04-15 13:00:09 +0200

    oops

--- _indexer/pageindex.mjs
+++ _indexer/pageindex.mjs
@@ -8,7 +8,7 @@ const go = () => {
     const files = Fs.readdirSync(dir);
     const flist = [];
     files.forEach((f) => {
-        if(/^Aink.+\.xml$/.test(f))
+        if(/^NA.+\.xml$/.test(f))
             flist.push(f);
     });
     
